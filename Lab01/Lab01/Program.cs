fd10586ac4ef0dfd2a630379a6cc517d9aeca580
Koson Trachu <koson.trachu@gmail.com>
2017-08-30 18:30:03 +0700

    lab for week 4

---
+++ Lab01/Lab01/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Lab01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double i = 123.456789d;
+            Console.WriteLine("{0:F1}",i);
+            Console.WriteLine("{0:F2}",i);
+            Console.WriteLine("{0:F3}",i);
+            Console.WriteLine("{0:F4}",i);
+            Console.WriteLine("{0:F5}", i);
+        }
+    }
+}
